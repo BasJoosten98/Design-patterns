9b55696bca6c40768c577a65092a0fb574c1c614
Joosten,Bas B.A.G.A <I399717@fhict.nl>
2019-04-19 14:35:49 +0200

    Form1 almost complete

--- DiskScheduling - Strategy Pattern/DiskScheduling/DiskScheduling/Form1.Designer.cs
+++ DiskScheduling - Strategy Pattern/DiskScheduling/DiskScheduling/Form1.Designer.cs
@@ -32,37 +32,86 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.removeItemsTimer = new System.Windows.Forms.Timer(this.components);
             this.runBtn = new System.Windows.Forms.Button();
+            this.rbScan = new System.Windows.Forms.RadioButton();
+            this.rbShortestSeekTimeFirst = new System.Windows.Forms.RadioButton();
+            this.rbFirstComeFirstServe = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(12, 22);
+            this.listBox1.Location = new System.Drawing.Point(6, 11);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(556, 604);
+            this.listBox1.Size = new System.Drawing.Size(280, 316);
             this.listBox1.TabIndex = 0;
+            // 
+            // removeItemsTimer
+            // 
+            this.removeItemsTimer.Tick += new System.EventHandler(this.removeItemsTimer_Tick);
             // 
             // runBtn
             // 
-            this.runBtn.Location = new System.Drawing.Point(706, 70);
+            this.runBtn.Location = new System.Drawing.Point(353, 36);
+            this.runBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.runBtn.Name = "runBtn";
-            this.runBtn.Size = new System.Drawing.Size(170, 56);
+            this.runBtn.Size = new System.Drawing.Size(85, 29);
             this.runBtn.TabIndex = 1;
             this.runBtn.Text = "Run";
             this.runBtn.UseVisualStyleBackColor = true;
             this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
             // 
+            // rbScan
+            // 
+            this.rbScan.AutoSize = true;
+            this.rbScan.Location = new System.Drawing.Point(584, 47);
+            this.rbScan.Name = "rbScan";
+            this.rbScan.Size = new System.Drawing.Size(50, 17);
+            this.rbScan.TabIndex = 2;
+            this.rbScan.TabStop = true;
+            this.rbScan.Text = "Scan";
+            this.rbScan.UseVisualStyleBackColor = true;
+            this.rbScan.CheckedChanged += new System.EventHandler(this.rbScan_CheckedChanged);
+            // 
+            // rbShortestSeekTimeFirst
+            // 
+            this.rbShortestSeekTimeFirst.AutoSize = true;
+            this.rbShortestSeekTimeFirst.Location = new System.Drawing.Point(584, 70);
+            this.rbShortestSeekTimeFirst.Name = "rbShortestSeekTimeFirst";
+            this.rbShortestSeekTimeFirst.Size = new System.Drawing.Size(140, 17);
+            this.rbShortestSeekTimeFirst.TabIndex = 3;
+            this.rbShortestSeekTimeFirst.TabStop = true;
+            this.rbShortestSeekTimeFirst.Text = "Shortest Seek Time First";
+            this.rbShortestSeekTimeFirst.UseVisualStyleBackColor = true;
+            this.rbShortestSeekTimeFirst.CheckedChanged += new System.EventHandler(this.rbShortestSeekTimeFirst_CheckedChanged);
+            // 
+            // rbFirstComeFirstServe
+            // 
+            this.rbFirstComeFirstServe.AutoSize = true;
+            this.rbFirstComeFirstServe.Location = new System.Drawing.Point(584, 93);
+            this.rbFirstComeFirstServe.Name = "rbFirstComeFirstServe";
+            this.rbFirstComeFirstServe.Size = new System.Drawing.Size(127, 17);
+            this.rbFirstComeFirstServe.TabIndex = 4;
+            this.rbFirstComeFirstServe.TabStop = true;
+            this.rbFirstComeFirstServe.Text = "First Come First Serve";
+            this.rbFirstComeFirstServe.UseVisualStyleBackColor = true;
+            this.rbFirstComeFirstServe.CheckedChanged += new System.EventHandler(this.rbFirstComeFirstServe_CheckedChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1515, 749);
+            this.ClientSize = new System.Drawing.Size(758, 389);
+            this.Controls.Add(this.rbFirstComeFirstServe);
+            this.Controls.Add(this.rbShortestSeekTimeFirst);
+            this.Controls.Add(this.rbScan);
             this.Controls.Add(this.runBtn);
             this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +119,9 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Timer removeItemsTimer;
         private System.Windows.Forms.Button runBtn;
+        private System.Windows.Forms.RadioButton rbScan;
+        private System.Windows.Forms.RadioButton rbShortestSeekTimeFirst;
+        private System.Windows.Forms.RadioButton rbFirstComeFirstServe;
     }
 }
 
--- DiskScheduling - Strategy Pattern/DiskScheduling/DiskScheduling/Form1.cs
+++ DiskScheduling - Strategy Pattern/DiskScheduling/DiskScheduling/Form1.cs
@@ -16,27 +16,53 @@ namespace DiskScheduling
         private const int NUMBER_OF_RANDOM_REQUESTS = 15;
         // Ints will be of request type.
         private List<Request> myRequests;
+        private OperatingSystem os;
         public Form1()
         {
             InitializeComponent();
             rng = new Random();
             // Generate a list of random requests based on set constant.
-            myInts = GenerateRandomRequests(NUMBER_OF_RANDOM_REQUESTS);
+            myRequests = GenerateRandomRequests(NUMBER_OF_RANDOM_REQUESTS);
         }
 
         private void runBtn_Click(object sender, EventArgs e)
         {
             // The timer object handles 1 request per tick and also adds one new request at the end.
+            removeItemsTimer.Interval = 200;
+            removeItemsTimer.Enabled = true;
         }
 
         private List<Request> GenerateRandomRequests(int numberOfRandomRequests)
         {
+            List<Request> tempList = new List<Request>();
             for (int i = 0; i < numberOfRandomRequests; i++)
             {
                 // Random int < 100, lets say it is possible for different processes
                 // to request access to the same sector, thus us not checking for uniqueness.
-                rng.Next(100);
+                Request req = new Request(rng.Next(100));
+                tempList.Add(req);
             }
+            return tempList;
+        }
+
+        private void rbScan_CheckedChanged(object sender, EventArgs e)
+        {
+            os.DiskschedulingMethod = new Scan();
+        }
+
+        private void rbShortestSeekTimeFirst_CheckedChanged(object sender, EventArgs e)
+        {
+            os.DiskschedulingMethod = new ShortestSeekTimeFirst();
+        }
+
+        private void rbFirstComeFirstServe_CheckedChanged(object sender, EventArgs e)
+        {
+            os.DiskschedulingMethod = new FirstComeFirstServe();
+        }
+
+        private void removeItemsTimer_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
