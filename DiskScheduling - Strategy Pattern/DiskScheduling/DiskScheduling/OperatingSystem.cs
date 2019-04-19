fe041d2194228ab99376cbf9416059b12d6254c2
Joosten,Bas B.A.G.A <I399717@fhict.nl>
2019-04-19 14:42:15 +0200

    OS Done

--- DiskScheduling - Strategy Pattern/DiskScheduling/DiskScheduling/OperatingSystem.cs
+++ DiskScheduling - Strategy Pattern/DiskScheduling/DiskScheduling/OperatingSystem.cs
@@ -6,34 +6,45 @@ namespace DiskScheduling
 {
 	public class OperatingSystem
 	{
+        private IDiskScheduling diskschedulingMethod;
+        private HardDisk disk;
+        private List<Request> requests;
 		public IDiskScheduling DiskschedulingMethod
 		{
 			get
 			{
-				throw new NotImplementedException();
+                return diskschedulingMethod;
 			}
 			set
 			{
-				throw new NotImplementedException();
+                diskschedulingMethod = value;
 			}
 		}
+        public List<Request> Requests
+        {
+            get
+            {
+                return requests;
+            }
+        }
 
-		public HardDisk Disk
+        public HardDisk Disk
 		{
 			get
 			{
-				throw new NotImplementedException();
+                return disk;
 			}
 		}
 
 		public void AddRequest(Request request)
 		{
-			throw new NotImplementedException();
+            requests.Add(request);
 		}
 
-		public OperatingSystem(HardDisk disk)
+		public OperatingSystem(HardDisk Disk)
 		{
-			throw new NotImplementedException();
+            disk = Disk;
+            requests = new List<Request>();
 		}
 	}
 }
