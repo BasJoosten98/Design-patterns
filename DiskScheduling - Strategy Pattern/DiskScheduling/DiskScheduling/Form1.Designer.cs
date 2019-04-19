5267ff6ed8358fa6beb28892a8e3cc7454fb968a
Remy Bos <remy.bos@student.fontys.nl>
2019-04-19 13:44:08 +0200

    Initial commit

---
+++ DiskScheduling - Strategy Pattern/DiskScheduling/DiskScheduling/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace DiskScheduling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.removeItemsTimer = new System.Windows.Forms.Timer(this.components);
+            this.runBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(556, 604);
+            this.listBox1.TabIndex = 0;
+            // 
+            // runBtn
+            // 
+            this.runBtn.Location = new System.Drawing.Point(706, 70);
+            this.runBtn.Name = "runBtn";
+            this.runBtn.Size = new System.Drawing.Size(170, 56);
+            this.runBtn.TabIndex = 1;
+            this.runBtn.Text = "Run";
+            this.runBtn.UseVisualStyleBackColor = true;
+            this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1515, 749);
+            this.Controls.Add(this.runBtn);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer removeItemsTimer;
+        private System.Windows.Forms.Button runBtn;
+    }
+}
+
---
+++ DiskScheduling - Strategy Pattern/DiskScheduling/DiskScheduling/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiskScheduling
+{
+    public partial class Form1 : Form
+    {
+        private Random rng;
+        private const int NUMBER_OF_RANDOM_REQUESTS = 15;
+        // Ints will be of request type.
+        private List<Request> myRequests;
+        public Form1()
+        {
+            InitializeComponent();
+            rng = new Random();
+            // Generate a list of random requests based on set constant.
+            myInts = GenerateRandomRequests(NUMBER_OF_RANDOM_REQUESTS);
+        }
+
+        private void runBtn_Click(object sender, EventArgs e)
+        {
+            // The timer object handles 1 request per tick and also adds one new request at the end.
+        }
+
+        private List<Request> GenerateRandomRequests(int numberOfRandomRequests)
+        {
+            for (int i = 0; i < numberOfRandomRequests; i++)
+            {
+                // Random int < 100, lets say it is possible for different processes
+                // to request access to the same sector, thus us not checking for uniqueness.
+                rng.Next(100);
+            }
+        }
+    }
+}
